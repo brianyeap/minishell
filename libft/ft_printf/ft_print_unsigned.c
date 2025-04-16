a8b943d21bc29969ff45099f3a97c0ffaadadb34
Brian yeap <brianyeap@hotmail.com>
2025-04-16 18:23:44 +0800

    fux unsigned

--- libft/ft_printf/ft_print_unsigned.c
+++ libft/ft_printf/ft_print_unsigned.c
@@ -3,21 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_unsigned.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bryeap <bryeap@student.42.fr>              +#+  +:+       +#+        */
+/*   By: brian <brian@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/28 14:26:12 by bryeap            #+#    #+#             */
-/*   Updated: 2024/06/28 15:51:11 by bryeap           ###   ########.fr       */
+/*   Updated: 2025/04/16 18:20:58 by brian            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-static unsigned int	u_abs_val(unsigned int nbr)
-{
-	if (nbr < 1)
-		nbr *= -1;
-	return (nbr);
-}
 
 static int	u_int_len(unsigned int nbr)
 {
@@ -36,16 +29,12 @@ static int	u_int_len(unsigned int nbr)
 
 static char	*u_ft_itoa(unsigned int n)
 {
-	int					sign;
 	int					len;
-	unsigned long long	nbr;
+	unsigned int		nbr;
 	char				*mem;
 
-	sign = 0;
-	if (n < 0)
-		sign = 1;
-	nbr = u_abs_val(n);
-	len = u_int_len(nbr) + sign;
+	nbr = n;
+	len = u_int_len(nbr);
 	mem = malloc(sizeof(char) * (len + 1));
 	if (!mem)
 		return (NULL);
@@ -55,8 +44,6 @@ static char	*u_ft_itoa(unsigned int n)
 		mem[len] = (nbr % 10) + '0';
 		nbr /= 10;
 	}
-	if (sign)
-		mem[0] = '-';
 	return (mem);
 }
 
