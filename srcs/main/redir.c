68e389ba860a2e4f5a10844d7b8a923f540b3092
Brian yeap <brianyeap@hotmail.com>
2025-04-30 05:19:06 +0800

    added comment

--- srcs/main/redir.c
+++ srcs/main/redir.c
@@ -6,7 +6,7 @@
 /*   By: brian <brian@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 17:41:14 by brian             #+#    #+#             */
-/*   Updated: 2025/04/30 01:21:39 by brian            ###   ########.fr       */
+/*   Updated: 2025/04/30 05:17:53 by brian            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -92,15 +92,7 @@ int	handle_heredoc(t_mini *mini, const char *delimiter, int expand)
 	char	*line;
 	char	*expanded;
 
-	if (pipe(pipefd) == -1)
-	{
-		ft_putstr_fd("minishell: ", STDERR);
-		perror("pipe");
-		mini->ret = 1;
-		mini->no_exec = 1;
-		return (-1);
-	}
-
+	pipe(pipefd);
 	while (1)
 	{
 		line = readline("heredoc ▸ ");
@@ -111,38 +103,30 @@ int	handle_heredoc(t_mini *mini, const char *delimiter, int expand)
 			ft_putendl_fd("')", STDERR);
 			break;
 		}
-
 		if (ft_strncmp(line, delimiter, ft_strlen(delimiter)) == 0
 			&& ft_strlen(line) == ft_strlen(delimiter))
 		{
 			free(line);
 			break;
 		}
-
 		if (expand)
 		{
-			ft_putstr_fd("EXPAND ", STDERR);
 			mark_expansions(line);
 			expanded = expansions(line, mini->env, mini->ret);
 		}
 		else
-		{
-			ft_putstr_fd("NO EXPAND ", STDERR);
 			expanded = ft_strdup(line);
-		}
-
-
 		if (expanded)
 		{
-			write(pipefd[1], expanded, ft_strlen(expanded));
+			write(pipefd[1], expanded, ft_strlen(expanded));  // write to write end
 			write(pipefd[1], "\n", 1);
 			free(expanded);
 		}
 		free(line);
 	}
 
-	close(pipefd[1]);
-	dup2(pipefd[0], STDIN_FILENO);
-	close(pipefd[0]);
+	close(pipefd[1]); // close write end cause we done with writting
+	dup2(pipefd[0], STDIN_FILENO); // Now anything that reads from STDIN (file descriptor 0) will get data from pipefd[0].  like E.G cat
+	close(pipefd[0]); // close read end since i already dup2 it and i don't need it anymore
 	return (0);
 }
