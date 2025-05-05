da3b5ae2bf6cca6175361a898f34eeb55fa572e3
Brian yeap <brianyeap@hotmail.com>
2025-05-05 15:53:56 +0800

    fix norm

--- srcs/env/sort_env.c
+++ srcs/env/sort_env.c
@@ -6,7 +6,7 @@
 /*   By: brian <brian@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 17:00:29 by brian             #+#    #+#             */
-/*   Updated: 2025/05/05 02:14:07 by brian            ###   ########.fr       */
+/*   Updated: 2025/05/05 15:53:38 by brian            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,7 +48,7 @@ void	sort_env(char **tab, int env_len)
 	}
 }
 
-void print_sorted_env_helper(char *eq, char *tab)
+void	print_sorted_env_helper(char *eq, char *tab)
 {
 	*eq = '\0';
 	ft_putstr("declare -x ");
